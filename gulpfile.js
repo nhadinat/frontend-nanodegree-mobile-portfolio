ee932c7cf3b00c918190059e7f3bb9bbbfcf715d
nhadinat <nathanhadinata@gmail.com>
2015-08-28 12:57:33 -0700

    Add src folder

--- gulpfile.js
+++ gulpfile.js
@@ -12,9 +12,9 @@ var gulp = require('gulp'),
 
 // Move jpgs Into Dist
 gulp.task('images', function() {
-  var portfolio = gulp.src('img/*.jpg')
+  var portfolio = gulp.src('src/img/*.jpg')
     .pipe(gulp.dest('dist/img'));
-  var pizza = gulp.src('views/images/*.jpg')
+  var pizza = gulp.src('src/views/images/*.jpg')
     .pipe(gulp.dest('dist/views/img'));
 
   return merge(portfolio, pizza);
@@ -22,10 +22,10 @@ gulp.task('images', function() {
 
 // Optimize PNGs
 gulp.task('png', function () {
-  var portfolio = gulp.src('img/*.png')
+  var portfolio = gulp.src('src/img/*.png')
     .pipe(imageminPngquant({quality: '65-80', speed: 4})())
     .pipe(gulp.dest('dist/img'));
-  var pizza = gulp.src('views/images/*.png')
+  var pizza = gulp.src('src/views/images/*.png')
     .pipe(imageminPngquant({quality: '65-80', speed: 4})())
     .pipe(gulp.dest('dist/views/img'));
 
@@ -39,10 +39,10 @@ gulp.task('html', function() {
     spare:true
   };
 
-  var portfolio = gulp.src('*.html')
+  var portfolio = gulp.src('src/*.html')
     .pipe(minifyHTML(opts))
     .pipe(gulp.dest('dist'));
-  var pizza = gulp.src('views/pizza.html')
+  var pizza = gulp.src('src/views/pizza.html')
     .pipe(minifyHTML(opts))
     .pipe(gulp.dest('dist/views'));
 
@@ -51,11 +51,11 @@ gulp.task('html', function() {
 
 // Concatenate And Minify JavaScript
 gulp.task('scripts', function(){
-  var portfolio = gulp.src('js/perfmatters.js')
+  var portfolio = gulp.src('src/js/perfmatters.js')
     .pipe(rename('perfmatters.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('dist/js'));
-  var pizza = gulp.src('views/js/main.js')
+  var pizza = gulp.src('src/views/js/main.js')
     .pipe(rename('main.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('dist/views/js'));
@@ -65,15 +65,15 @@ gulp.task('scripts', function(){
 
 // Concatenate And Minify CSS
 gulp.task('styles', function(){
-  var style = gulp.src('css/style.css')
+  var style = gulp.src('src/css/style.css')
     .pipe(rename('style.min.css'))
     .pipe(minifyCSS())
     .pipe(gulp.dest('dist/css'));
-  var print = gulp.src('css/print.css')
+  var print = gulp.src('src/css/print.css')
     .pipe(rename('print.min.css'))
     .pipe(minifyCSS())
     .pipe(gulp.dest('dist/css'));
-  var pizzastyle = gulp.src('views/css/*.css')
+  var pizzastyle = gulp.src('src/views/css/*.css')
     .pipe(concatCss('pizza.min.css'))
     .pipe(minifyCSS())
     .pipe(gulp.dest('dist/views/css'));
@@ -82,7 +82,7 @@ gulp.task('styles', function(){
 });
 
 // Default
-gulp.task('default', ['images', 'html', 'scripts', 'styles']);
+gulp.task('default', ['images', 'png', 'html', 'scripts', 'styles']);
 
 // Publish to gh-pages
 gulp.task('deploy', function() {
